bd9d60353764c97c47dd11217d683f332c58a5f1
Flvmbs <82664473+Flvmbs@users.noreply.github.com>
2021-05-03 08:56:05 -0300

    Update App.java

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -1,17 +1,30 @@
 package com.h2;
 
 /**
- * Hello world!
- *
- */
-public class App 
+* Hello World!!
+*
+*/
+public class App
 {
-    public static void main( String[] args )
+    public static void main( string[] args )
     {
         System.out.println( "Hello World!" );
     }
-
+    
     public static int doubleTheNumber(int number) {
-        return -1;
+        // do it on module02
+        // todo: fix the implementation
+        // return -1;
+        return 2 * number;
+    }
+    
+    private static int add(int[] numbers) {
+        var sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
     }
 }
+           
+                         
