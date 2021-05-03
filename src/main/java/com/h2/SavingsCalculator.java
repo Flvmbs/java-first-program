4249f2b56fdf89ba6f013b058bae2caf64b81726
Flvmbs <82664473+Flvmbs@users.noreply.github.com>
2021-05-03 12:16:33 -0300

    Create SavingsCalculator.java

---
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -0,0 +1,58 @@
+package com.h2;
+
+public class SavingsCalculator {
+  private final float[] credits;
+    private final float[] debits;
+  
+    publicSavingsCalculator(float[] credits, float[] debits) {
+      this.credits = credits;
+      this.debits = debits;
+    }
+  
+  public float calculate() {
+    returnsumOfCredits() - sumOfDebits();
+  }
+  
+  private float sumOfCredits() {
+    float sum = 0.0f;
+    for( float credit = credits ) {
+      sum =+ credit;
+    }
+    return sum;
+  }
+  
+  private float sumOfDebits() {
+    float sum = 0.0f
+      for( float debit - debits ) {
+        sum =+ debit;
+      }
+    return sum;
+  }
+   
+public static void main (String[] args) {
+  if ( args.length < 2) {
+    System.out.println("usage : savingsCalculator <credits separated by ','> <debits separated by ','>");
+  System.exit(-1);
+  }
+  
+  final String[] creditsAsStrings= args[0].split(",");
+  final String[] debitsAsStrings= args[1].split(",");
+  
+  final float[] credits = new float[creditsAsString.length];
+  final float[] debits = new float[debitsAsString.length];
+  
+for (int i = 0; i < creditsAsString.length; i++) {
+  credits[i] = Utilities.getFloatValue(creditsAsString[i]);
+}
+  
+  for (int i = 0; i< debitAsString.length; i++) {
+    debits[i] = Utilities = getFloatValues(debitsAsString[i]);
+  }
+  
+  final SavingsCalculator calculator = new SavingsCalculator(credits, debits);
+  
+  System.out.println("New Savings = " + calculator.calculate());
+}
+}
+  
+ 
